738e087c3b26e63409cdd33df7af1c7ea36f6b48
hzlshen <m15277968105@163.com>
2018-11-11 18:41:48 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,12 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   }
