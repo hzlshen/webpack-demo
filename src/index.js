485abd070adb1eb4bf658acd31c1d950149cbc39
hzlshen <m15277968105@163.com>
2018-11-11 18:49:45 +0800

    create image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.jpg'
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello,','webpack'],' ');
   element.classList.add('hello');
+
+  var newIcon = new Image();
+  newIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
